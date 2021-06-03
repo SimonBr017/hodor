0e5242abe01f5e1b9afb154e0fe17e98069653eb
SimonBr017 <simonbrard017@gmail.com>
2021-06-03 18:15:43 +0200

    litlle fix

--- level_4/level_04_back.py
+++ level_4/level_04_back.py
@@ -51,7 +51,7 @@ head = {'Referer': url, 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64;\
  x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141\
  Safari/537.36 Edg/87.0.664.75"}
 
-while repeat <= 98:
+while repeat < 98:
     proxy = next(proxy_pool)
     print("Request #%d" % i)
     try:

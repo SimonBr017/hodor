4d05a660813e0c7c89a38ce76a8e7be4a4bd718b
SimonBr017 <simonbrard017@gmail.com>
2021-06-03 17:24:02 +0200

    final version

--- level_4/level_04_back.py
+++ level_4/level_04_back.py
@@ -7,24 +7,31 @@ from itertools import cycle
 
 from requests.models import Response
 
-def get_proxies():
 
+def get_proxies():
     url = 'https://free-proxy-list.net/'
     response = requests.get(url)
     parser = fromstring(response.text)
     proxies = set()
     for i in parser.xpath('//tbody/tr'):
         if i.xpath('.//td[7][contains(text(),"yes")]'):
-            # Grabbing IP and corresponding PORT
             proxy = ":".join([i.xpath('.//td[1]/text()')[0],
                               i.xpath('.//td[2]/text()')[0]])
             proxies.add(proxy)
-
+    url2 = 'https://www.us-proxy.org/'
+    response = requests.get(url2)
+    parser = fromstring(response.text)
+    for i in parser.xpath('//tbody/tr'):
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0],
+                              i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
     return proxies
+
 
 def get_vote():
     url = 'http://158.69.76.135/level4.php'
-    content= requests.get(url).text
+    content = requests.get(url).text
     parser = fromstring(content)
 
     for i in parser.xpath('//table/tr'):
@@ -33,8 +40,8 @@ def get_vote():
 
     return int(vote)
 
-repeat = 0
 
+repeat = 0
 proxies = get_proxies()
 proxy_pool = cycle(proxies)
 url = "http://158.69.76.135/level4.php"
@@ -44,44 +51,34 @@ head = {'Referer': url, 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64;\
  x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141\
  Safari/537.36 Edg/87.0.664.75"}
 
-
-
 while repeat <= 98:
-    
-    
-    
     proxy = next(proxy_pool)
-    print("Request #%d"%i)
+    print("Request #%d" % i)
     try:
         vote = get_vote()
         print("vote:{}".format(repeat))
-        print("vote reel: {}".format(vote))
+        print("vote real count: {}".format(vote))
         print(proxy)
         s = requests.session()
-
         r = s.get(url)
         doc = r.content
-
         soup = BeautifulSoup(doc, 'html.parser')
-
         key = soup.find("input", {"name": "key"})
         key_value = key.get("value")
-        
         s.proxies = {"http": "http://" + proxy, "https": "http://" + proxy}
         result = s.post(url, headers=head,
-                        data={"id": id, "holdthedoor": 1, "key": key_value}, timeout=5)
-        
+                        data={"id": id, "holdthedoor": 1,
+                              "key": key_value}, timeout=5)
         new_vote = get_vote()
-        print("vote normalement reel {}".format(vote))
+        print("last real vote count: {}".format(vote))
         print("new_vote: {}".format(new_vote))
-       
         if new_vote == vote + 1:
             repeat += 1
             print("Sucsess !!!\n")
+        else:
+            print("Fail to vote\n")
         i += 1
-        print("Fail to vote\n")
         s.cookies.clear()
-        
     except:
         i += 1
         s.cookies.clear()
